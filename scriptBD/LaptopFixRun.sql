553957366a7e4cae7511646e37e2a2a7422ab9af
JorgeCoronelG <JorgeCoronelG@users.noreply.github.com>
2019-05-20 09:34:19 -0500

    Proyecto terminado 1.0

---
+++ scriptBD/LaptopFixRun.sql
@@ -0,0 +1,57 @@
+/**
+ * Author:  tprog
+ * Created: 20/05/2019
+ */
+
+CREATE DATABASE LFR;
+USE LFR;
+
+--Tabla para los tipos de usuario
+CREATE TABLE TYPE_USER(
+    idTypeUser int(1) NOT NULL PRIMARY KEY,
+    typeUser varchar(50)
+);
+
+INSERT INTO TYPE_USER VALUES(1, 'LF');
+INSERT INTO TYPE_USER VALUES(2, 'Cliente');
+
+--Tabla de información del usuario para accesar a la aplicación
+CREATE TABLE USER(
+    email varchar(120) NOT NULL PRIMARY KEY,
+    password varchar(32),
+    status int(1),
+    idTypeUser int(1),
+    FOREIGN KEY(idTypeUser) REFERENCES TYPE_USER(idTypeUser)
+);
+
+INSERT INTO USER VALUES('laptopfix@gmail.com',md5('123456'),1,1);
+
+--Tabla del cliente para guardar su información
+CREATE TABLE CUSTOMER(
+    idCus int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nameCus varchar(150),
+    numberCus varchar(10),
+    email varchar(120),
+    FOREIGN KEY(email) REFERENCES USER(email)
+);
+
+--Tabla para guardar las citas que agende el cliente
+CREATE TABLE DATE_CUSTOMER(
+    idDate int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    idCus int,
+    dateCus date,
+    hourCus time,
+    residenceCus text,
+    descProblem text,
+    FOREIGN KEY(idCus) REFERENCES CUSTOMER(idCus)
+);
+
+--Tabla para guardar los comentarios que hagan los clientes
+CREATE TABLE COMMENT(
+    idComment int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    comment text,
+    score int(1),
+    dateComment date,
+    idCus int,
+    FOREIGN KEY(idCus) REFERENCES CUSTOMER(idCus)
+);
